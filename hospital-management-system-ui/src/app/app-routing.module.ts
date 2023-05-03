c4d6b473529d469aed5d5304f64bcb1844700940
Abhijit Bhangale <abhijitbhangale3@gmail.com>
2023-05-04 00:35:43 +0530

    day 3 - Angular components added with some typescript code

--- hospital-management-system-ui/src/app/app-routing.module.ts
+++ hospital-management-system-ui/src/app/app-routing.module.ts
@@ -3,14 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { StaffComponent } from './staff/staff.component';
 import { AboutComponent } from './about/about.component';
-import { LoginFormComponent } from './login-form/login-form.component';
+import { LoginFormComponent } from './Admin/login-form/login-form.component';
+import { RecordsComponent } from './Admin/records/records.component';
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'home', component:HomeComponent},
   {path:'staff', component:StaffComponent},
   {path:'about', component:AboutComponent},
-  {path:'login-form', component:LoginFormComponent}
+  {path:'login-form', children:[
+  {path:'', component:LoginFormComponent},
+  {path:'records', component:RecordsComponent}
+]}
 ];
 
 @NgModule({
